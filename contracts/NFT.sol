b80f732dcc5bb446f0c8a45d7fdcfab925aeb1b3
superman <goodnesskolapo@gmail.com>
2023-02-16 03:26:08 +0100

    sucks

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,7 +1,49 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import {IERC721} from "./INFT.sol";
 
-contract NFT {
-    
+/**
+ * requirements:
+ * name
+ * symbol
+ * id
+ */
+
+contract NFT{
+
+    string public  name;
+    string public  symbol;
+
+    uint256 Id;
+    mapping (address => uint256)  tokenholder;
+
+
+    // Implement the required functions from the interface here
+    function balanceOf(address owner) external view  returns (uint256 balance) {
+        // ...
+    }
+
+    function ownerOf(uint256 tokenId) external view  returns (address owner) {
+        // ...
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) external  {
+        // ...
+    }
+
+    function approve(address approved, uint256 tokenId) external  {
+        // ...
+    }
+
+    function getApproved(uint256 tokenId) external view  returns (address approved) {
+        // ...
+    }
+
+    function setApprovalForAll(address operator, bool _approved) external  {
+        // ...
+    }
+
+    function isApprovedForAll(address owner, address operator) external view  returns (bool) {
+        // ...
+    }
 }

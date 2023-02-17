66d2e30f4868a5faf4a3960a60e66380bdb5c125
superman <goodnesskolapo@gmail.com>
2023-02-17 04:26:52 +0100

    damn

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,31 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-etherscan";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-
-  networks: {
-
-      hardhat: {
-        goerli: {
-            url: process.env.GOERLI_RPC,
-        },
-        forking: {
-          //@ts-ignore
-          url: process.env.MAINETURL,
-        }
-      },
-  },
-  etherscan: {
-    //@ts-ignore
-    api_key: process.env.ETHERSCAN_API_KEY,
-},
-
-
 };
 
 export default config;
